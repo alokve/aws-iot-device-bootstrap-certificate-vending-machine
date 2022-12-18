be4ff89d5d91a0f096b06f98ac25db539a12c8d8
alokve <101735395+alokve@users.noreply.github.com>
2022-12-18 21:03:46 +0100

    Initial commit

---
+++ test/aws-iot-device-bootstrap-certificate-vending-machine.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsIotDeviceBootstrapCertificateVendingMachine from '../lib/aws-iot-device-bootstrap-certificate-vending-machine-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-iot-device-bootstrap-certificate-vending-machine-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsIotDeviceBootstrapCertificateVendingMachine.AwsIotDeviceBootstrapCertificateVendingMachineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
